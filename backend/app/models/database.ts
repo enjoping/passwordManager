540a634b59b73787c3f240dc7740bebb03502272
Joe Pietler <pietler@brightsolutions.de>
2017-06-01 17:12:19 +0200

    Implement the database class which manages the database connection and move the schema definitions into *Model.ts files

---
+++ backend/app/models/database.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by Joe Pietler on 01.06.17.
+ */
+
+import * as mongoose from "mongoose";
+
+export class Database {
+    public static getInstance(): Database {
+        return Database.instance;
+    }
+
+    private static instance: Database = new Database();
+
+    constructor() {
+        if (Database.instance) {
+            throw new Error("Error: Instantiation failed: Use .getInstance() instead of new.");
+        }
+        Database.instance = this;
+    }
+
+    public connect(database: any) {
+        mongoose.connect(database);
+        (mongoose as any).Promise = global.Promise;
+    }
+}
