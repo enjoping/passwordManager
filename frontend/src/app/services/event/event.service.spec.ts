add693fff0d5c68926154c2dd52718f5a39048fa
Lukas <lukas.schardt@stud.h-da.de>
2017-06-02 11:27:29 +0200

    wrote tests for the event service and fixed old app component tests

---
+++ frontend/src/app/services/event/event.service.spec.ts
@@ -0,0 +1,39 @@
+import {TestBed, async, inject} from '@angular/core/testing';
+import EventService from './event.service';
+import {EventReceiver} from './event-receiver.interface';
+
+class FakeEventReceiver implements EventReceiver {
+  onEventReceived(source, key, value) {
+    expect(key).toEqual('fake-key');
+  }
+}
+
+class RealEventReceiver implements EventReceiver {
+  onEventReceived(source, key, value) {
+    expect(key).not.toEqual('real-key');
+  }
+}
+
+describe('EventService', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        EventService
+      ]
+    });
+  }));
+
+  it('should inject the service', inject([ EventService ], (eventService: EventService) => {
+    expect(eventService).toBeDefined();
+  }));
+
+  it('should call the event receivers', async(inject([ EventService ], (eventService: EventService) => {
+    const fakeEventReceiver = new FakeEventReceiver();
+    eventService.subscribe(fakeEventReceiver);
+
+    const realEventReceiver = new RealEventReceiver();
+    eventService.subscribe(realEventReceiver);
+
+    eventService.inform(this, 'fake-key', 'fake information');
+  })));
+});
