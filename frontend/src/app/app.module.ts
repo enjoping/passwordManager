73bd391d608fd6481852a9d90b2dc2a63c3ec42f
Lukas <lukas.schardt@stud.h-da.de>
2017-06-02 10:43:46 +0200

    implemented sentry.io error logging and created an event service for handling events between components/services

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {ErrorHandler, NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import GroupService from './services/group.service';
+import EventService from './services/event/event.service';
+import * as Raven from 'raven-js';
+
+
+
+Raven
+  .config('https://46c8374abe074351a3639dc38d93efd4@sentry.io/175238')
+  .install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err:any) : void {
+    Raven.captureException(err.originalError);
+  }
+}
 
 @NgModule({
     declarations: [
@@ -20,11 +34,13 @@ import GroupService from './services/group.service';
     /*
       ERROR thrown: Cannot read property 'provide' of null. App worked fine, even with the error thrown, but
       CI crashed.
-      Never seen it before. Fixed it with there instructions by changing the providers to the following format:
+      Never seen it before. Fixed it with these instructions by changing the providers to the following format:
         https://github.com/angular/angular-cli/issues/3834
      */
     providers: [
-      { provide: GroupService, useClass: GroupService }
+      { provide: GroupService, useClass: GroupService },
+      { provide: EventService, useClass: EventService },
+      { provide: ErrorHandler, useClass: RavenErrorHandler }
     ],
     bootstrap: [AppComponent]
 })
