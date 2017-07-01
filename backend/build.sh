d446c71088967a7cf6eb10be547c6f9ff540405f
Joe Pietler <pietler@brightsolutions.de>
2017-07-01 17:23:49 +0200

    Correct the build.sh file so the frontend will be in the correct folder.

--- backend/build.sh
+++ backend/build.sh
@@ -8,7 +8,7 @@ cp ./docker-compose.yml ./build/
 cp ./init.sh ./build/
 mkdir ./build/application/config
 cp ./config/build.json ./build/application/config/default.json
-mkdir ./build/application/dist
-cp -r ./frontend/ ./build/application/dist
+cp -r ./frontend ./build/application
+mv ./build/application/frontend ./build/application/dist
 cp -r ./installation ./build/application/
 tar czf build.tar.gz ./build
